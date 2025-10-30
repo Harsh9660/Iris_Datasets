44820444c19f8b2c845dd98f63e4d1ffa3ec3aba
harsh pandya <harsh.pandya@vrinsoft.com>
2025-10-30 15:29:39 +0530

    Updated

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
-from matplotlib.pyplot import plt 
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt 
 import seaborn as sns
 
 path = "Iris.csv"
