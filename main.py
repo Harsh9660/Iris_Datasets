51f1f89a1e603cdc6ee47ec9eb9f740bbe7d6b12
harsh pandya <harsh.pandya@vrinsoft.com>
2025-10-30 15:43:24 +0530

    Updated

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
-from matplotlib.pyplot import plt 
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 
 path = "Iris.csv"
+target_col = 'Species'
 
 df = pd.read_csv(path)
+
 print(df.head())
+
+print("\n Dataset Info:")
+print(df.info())
+
+print("\n Species Summary:")
+print(df[target_col].value_counts())
+
+print("\n Missing data from the datasets:", df.isnull().sum())
